6e8cf532fb84d94fee15264a07d7b56e298ce4fb
qingyi.liu <qingyi.liu@17zuoye.com>
2021-07-12 14:12:00 +0800

    test Hook 3

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const http = require("http");
 
-console.log(process.argv);
+// console.log(process.argv);
 
-console.log(process.argv[2], process.argv[3]);
+const paramsArr = process.argv.splice(2);
+
+let str = paramsArr.reduce((p, val) => {
+  if(val.startsWith("date")) {
+    const [name, value = ""] = val.split("=");
+    return p + name + "=" + new Date(value.replace("Date:", "").trim()).getTime();
+  }
+  return p + val + "&"
+}, "")
+
+
+let url = `http://10.197.6.240:3000/autoBuild?appName=karp_dipper&${str}`
+
+console.log(url);
+
+http.get(url)
+
+
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 userName="$(git config user.name)"
 branchName="$(git symbolic-ref --short HEAD)"
 Author="$(git log --author="$userName"  -1 HEAD | grep Author)"
-Date="$(git log -1 HEAD | grep Date)"
+Date="$(git log --author="$userName" -1 HEAD | grep Date)"
 
 
-echo "$(node script.js  userName=$userName branchName=$branchName Author="$Author" Date="$Date")"
+echo "$(node script.js  userName=$userName branchName=$branchName date="$Date")"
